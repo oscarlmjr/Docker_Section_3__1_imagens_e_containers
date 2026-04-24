c58e25409b6ad35c03acc14b1ea939c6aaf282d7
oscarlmjr <oscarlmjr@gmail.com>
2026-04-23 21:22:26 -0300

    secao_3 aula_44-52

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Olá minha imagem!')
+  res.send('Olá minha imagem')
 })
 
 app.listen(port, () => {
   console.log(`Executando na porta: ${port}`)
-});
+});45
